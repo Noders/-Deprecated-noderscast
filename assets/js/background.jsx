5c5a939ce10a02f245a89051561d42f57fb2b6bd
fforres <fetorres@groupon.com>
2015-08-26 10:13:42 -0300

    adding image and react-imageloader

--- assets/js/background.jsx
+++ assets/js/background.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var ImageLoader = require('react-imageloader')
+function preloader() {
+  return <img src="/assets/img/spinner.gif" />;
+}
 module.exports =  React.createClass({	
   	render: function() {
         return (
-        	<div className="background"> 
-        	</div>
+        	<ImageLoader 
+        		src='/assets/img/hosts1.JPG'
+        		className="background"
+        		wrapper={React.DOM.div}
+        		preloader={preloader} 
+        		>
+        		Carga de imagen fallida 
+        	</ImageLoader>
         );
   	}
 });
